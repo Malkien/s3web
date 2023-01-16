ecb855fa57a439ede48e17d7579d27f36a44e482
Malkien <kevin1cuenca@gmail.com>
2023-01-16 19:11:13 +0100

    changes upload

--- src/main/java/com/web/app/utils/S3Util.java
+++ src/main/java/com/web/app/utils/S3Util.java
@@ -5,9 +5,11 @@ import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
 import software.amazon.awssdk.awscore.exception.AwsServiceException;
 import software.amazon.awssdk.core.exception.SdkClientException;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
 import software.amazon.awssdk.services.s3.model.ListObjectsResponse;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Object;
 import software.amazon.awssdk.transfer.s3.S3TransferManager;
 import software.amazon.awssdk.transfer.s3.model.CompletedFileUpload;
@@ -24,18 +26,16 @@ public class S3Util {
     private static final String BUCKET_NAME = "my-bucket-practice";
     private static final S3Client s3= S3Client.builder().credentialsProvider(InstanceProfileCredentialsProvider.builder().build()).build();
     private static final S3TransferManager transferManager = S3TransferManager.create();
-    public static String uploadFile(String name, MultipartFile file){
+    public static String uploadFile(String key, MultipartFile file){
         try{
-            UploadFileRequest uploadFileRequest =
-                    UploadFileRequest.builder()
-                            .putObjectRequest(b -> b.bucket(BUCKET_NAME).key(name))
-                            .addTransferListener(LoggingTransferListener.create())
-                            .source(file.getResource().getFile())
-                            .build();
-            FileUpload fileUpload = transferManager.uploadFile(uploadFileRequest);
+            PutObjectRequest objectRequest = PutObjectRequest.builder()
+                    .bucket(BUCKET_NAME)
+                    .key(key)
+                    .build();
 
-            CompletedFileUpload uploadResult = fileUpload.completionFuture().join();
-            return uploadResult.response().eTag();
+            s3.putObject(objectRequest, RequestBody.fromInputStream(file.getInputStream(),file.getSize()));
+            return "Image Upload";
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
