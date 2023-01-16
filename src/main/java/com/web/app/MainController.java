4356127f938f3d3ec721bbe00f0b93872d666c39
Malkien <kevin1cuenca@gmail.com>
2023-01-16 12:48:16 +0100

    again trying create5 change to v2

--- src/main/java/com/web/app/MainController.java
+++ src/main/java/com/web/app/MainController.java
@@ -1,6 +1,5 @@
 package com.web.app;
 
-import com.amazonaws.services.s3.model.ObjectMetadata;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -9,8 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
--- src/main/java/com/web/app/S3Util.java
+++ src/main/java/com/web/app/S3Util.java
@@ -1,35 +1,34 @@
 package com.web.app;
 
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.transfer.s3.S3TransferManager;
+import software.amazon.awssdk.transfer.s3.model.CompletedFileUpload;
+import software.amazon.awssdk.transfer.s3.model.FileUpload;
+import software.amazon.awssdk.transfer.s3.model.UploadFileRequest;
+import software.amazon.awssdk.transfer.s3.progress.LoggingTransferListener;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
+import java.nio.file.Paths;
+
 @Service
 public class S3Util {
     private static final String BUCKET_NAME = "my-bucket-practice";
-    private static final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
+    private static final S3TransferManager transferManager = S3TransferManager.create();
     public static String uploadFile(String name, MultipartFile file){
         try{
-            ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentLength(file.getSize());
-            metadata.setContentType(file.getContentType());
-            s3.putObject(new PutObjectRequest(BUCKET_NAME, name, file.getInputStream(), metadata));
-        }catch (AmazonServiceException e){
-            System.err.println(e.getErrorMessage());
+            UploadFileRequest uploadFileRequest =
+                    UploadFileRequest.builder()
+                            .putObjectRequest(b -> b.bucket(BUCKET_NAME).key(name))
+                            .addTransferListener(LoggingTransferListener.create())
+                            .source(file.getResource().getFile())
+                            .build();
+            FileUpload fileUpload = transferManager.uploadFile(uploadFileRequest);
+
+            CompletedFileUpload uploadResult = fileUpload.completionFuture().join();
+            return uploadResult.response().eTag();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        s3.setObjectAcl(BUCKET_NAME, name, CannedAccessControlList.PublicRead);
-        return s3.getUrl(BUCKET_NAME, name).toString();
     }
 }
