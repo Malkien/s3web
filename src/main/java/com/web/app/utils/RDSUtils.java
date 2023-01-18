caf84506423050e50609746173ad6f8650431f87
Malkien <kevin1cuenca@gmail.com>
2023-01-18 16:35:13 +0100

    use ddbb

--- src/main/java/com/web/app/utils/RDSUtils.java
+++ src/main/java/com/web/app/utils/RDSUtils.java
@@ -21,7 +21,7 @@ public class RDSUtils {
     private static final String DB_USER = "admin";
     private static final String DATABASE = "images";
     private static final int RDS_INSTANCE_PORT = 3306;
-    private static final String JDBC_URL = "jdbc:mysql://" + RDS_INSTANCE_HOSTNAME + ":" + RDS_INSTANCE_PORT;
+    private static final String JDBC_URL = "jdbc:mysql://" + RDS_INSTANCE_HOSTNAME + ":" + RDS_INSTANCE_PORT + "/" + DATABASE;
 
 
 
